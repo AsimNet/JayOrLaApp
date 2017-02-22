03c1573f17f4b09c44d34bb14be7898a903aeaa5
AsimNet <hlh132132@gmail.com>
2017-02-23 02:58:44 +0300

    alert when you want to delete event

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Event } from '../../models/event';
-import { NavController, ModalController, Platform } from 'ionic-angular';
+import { NavController, ModalController, Platform, AlertController } from 'ionic-angular';
 import { Events } from '../../providers/events';
 import { AddNewEventPage } from '../add-new-event/add-new-event';
 import { Database } from '../../providers/database';
@@ -18,32 +18,33 @@ export class HomePage {
     public modalCtrl: ModalController,
     public events: Events,
     public translate: TranslateService,
+    public alertCtrl: AlertController,
     public database: Database,
     public platform: Platform) {
 
   }
 
-ionViewWillLeave(){
-  this.eventsList = [];
-}
+  ionViewWillLeave() {
+    this.eventsList = [];
+  }
 
-loadEventsList(){
-   this.platform.ready().then(() => {
+  loadEventsList() {
+    this.platform.ready().then(() => {
 
       this.database.getEvents().then((results) => {
-        
+
         this.eventsList = results;
       })
     })
-}
+  }
   ionViewWillEnter() {
-   this.loadEventsList()
+    this.loadEventsList()
   }
 
   addNewEvent() {
     let addEventPage = this.modalCtrl.create(AddNewEventPage);
-    addEventPage.onWillDismiss(()=>{
-   this.loadEventsList()
+    addEventPage.onWillDismiss(() => {
+      this.loadEventsList()
     })
     addEventPage.present();
 
@@ -54,11 +55,24 @@ loadEventsList(){
 
   }
 
-  delete(event: Event){
+  delete(event: Event) {
     //user wants to delete an event from the list
-    this.database.deleteEvent(event).then(()=>{
-      let eventIndex = this.eventsList.indexOf(event);
-      this.eventsList.splice(eventIndex,1)
-    })
+    let alert = this.alertCtrl.create({
+      title: this.translate.instant("WARNING"),
+      subTitle: this.translate.instant("YOU_WANT_DELETE") + " " + event.getName,
+      buttons: [{
+        text: this.translate.instant("CANCEL_BUTTON")
+      },
+      {
+        text: this.translate.instant("DELETE_BUTTON"),
+        handler: () => {
+          this.database.deleteEvent(event).then(() => {
+            let eventIndex = this.eventsList.indexOf(event);
+            this.eventsList.splice(eventIndex, 1);
+          })
+        }
+      }]
+    });
+    alert.present();
   }
 }
